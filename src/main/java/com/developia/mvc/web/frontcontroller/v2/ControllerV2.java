8b460a40b6c90257ada16177fbdbb994b8934014
developiaa <developiaa@gmail.com>
2021-10-15 22:45:01 +0900

    feat : Separate view render

--- src/main/java/com/developia/mvc/web/frontcontroller/v2/ControllerV2.java
+++ src/main/java/com/developia/mvc/web/frontcontroller/v2/ControllerV2.java
@@ -1,2 +1,12 @@
-package com.developia.mvc.web.frontcontroller.v2;public interface ControllerV2 {
+package com.developia.mvc.web.frontcontroller.v2;
+
+import com.developia.mvc.web.frontcontroller.MyView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface ControllerV2 {
+    MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 }
