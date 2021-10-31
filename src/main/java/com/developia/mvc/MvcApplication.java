595cad1041c5602ad6ac5036a427441ebbadeb09
developiaa <developiaa@gmail.com>
2021-10-31 15:19:38 +0900

    feat : Add view resolver

--- src/main/java/com/developia/mvc/MvcApplication.java
+++ src/main/java/com/developia/mvc/MvcApplication.java
@@ -3,6 +3,8 @@ package com.developia.mvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan   //서블릿 자동 등록
 @SpringBootApplication
@@ -12,4 +14,9 @@ public class MvcApplication {
         SpringApplication.run(MvcApplication.class, args);
     }
 
+//    spring boot가 자동적으로 해줌
+//    @Bean
+//    InternalResourceViewResolver internalResourceViewResolver(){
+//        return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//    }
 }
