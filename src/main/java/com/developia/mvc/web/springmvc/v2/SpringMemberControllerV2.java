426b75f03a4fd285c01da1241849e109d941b252
developiaa <developiaa@gmail.com>
2021-10-31 16:18:56 +0900

    chore : Fix words

--- src/main/java/com/developia/mvc/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/com/developia/mvc/web/springmvc/v2/SpringMemberControllerV2.java
@@ -23,7 +23,7 @@ public class SpringMemberControllerV2 {
     @RequestMapping("/save")
     public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
 
-        String username = request.getParameter("username");0
+        String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
 
         Member member = new Member(username, age);
